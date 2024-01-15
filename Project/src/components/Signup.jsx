f562c2f97028117a65717a8cc958585e9dcc5937
JatinShivlani <--global>
2024-01-15 18:21:02 +0530

    .

--- Project/src/components/Signup.jsx
+++ Project/src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
- 
+
 const Signup = (props) => {
     const navigate = useNavigate();
     const [authInput, changeAuthInput] = useState({
